99ac55343b67ae37e92b2159b8622df1667acb6c
shubham-tam <tamrakarshubham23@gmail.com>
2023-02-19 23:05:24 +0545

    Working on UI

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,6 +3,7 @@ import Add from "../assets/icons/add.png";
 import More from "../assets/icons/more.png";
 
 import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = () => {
   return (
@@ -16,6 +17,7 @@ const Chat = () => {
         </div>
       </div>
       <Messages />
+      <Input />
     </div>
   );
 };
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import Img from "../assets/icons/img.png";
+import Attach from "../assets/icons/attach.png";
+
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="input-send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
