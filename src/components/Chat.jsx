025c1c351f1723092b03eed76be394a5af2ff4c8
shubham-tam <tamrakarshubham23@gmail.com>
2023-02-19 22:42:21 +0545

    Working on the UI

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Camera from "../assets/icons/cam.png";
+import Add from "../assets/icons/add.png";
+import More from "../assets/icons/more.png";
+
+import Messages from "./Messages";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat-info">
+        <span>Jane</span>
+        <div className="chat-icons">
+          <img src={Camera} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Messages />
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="search__userChat">
+        <img
+          src="https://images.pexels.com/photos/15579683/pexels-photo-15579683.jpeg"
+          alt=""
+        />
+        <div className="search__userChat-info">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
